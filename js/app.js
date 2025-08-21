f948ee57020ba30452c73cd11c56ebf3c3a9c5bf
Programmer Rakibul <rakibul00206@gmail.com>
2025-08-22 01:36:46 +0600

    Cards border issue solved

--- js/app.js
+++ js/app.js
@@ -1,53 +1,75 @@
+// DOM Selectors
+const form = document.querySelector(".transaction-form");
+const button = document.getElementById("transaction-btn");
 const descEl = document.getElementById("desc");
 const amountEl = document.getElementById("amount");
 const totalBalanceEl = document.getElementById("total-balance");
 const totalIncomeEl = document.getElementById("total-income");
 const totalExpenseEl = document.getElementById("total-expense");
 const cardsContainer = document.querySelector(".transaction-lists");
+
+// Initializing income and expense variable
 let totalIncome = 0;
 let totalExpense = 0;
 
+// Making transaction cards based on input
 function makeCards(desc, amount) {
-  const cards = `
-            <li class="trans-child">
+  const childCards = `
+            <li class="">
               <span>${desc}</span>
               <span>$${amount}</span>
             </li>
   `;
+  const li = document.createElement("li");
+  li.classList.add("trans-child");
 
-  cardsContainer.innerHTML += cards;
+  if (amount < 0) {
+    li.classList.add("border-red");
+  } else {
+    li.classList.add("border-green");
+  }
+
+  li.innerHTML = childCards;
+
+  cardsContainer.appendChild(li);
 }
 
+// Function for listener
 function addTransaction() {
   const descVal = descEl.value;
-  let amountVal = amountEl.value;
-  amountVal = Number.parseFloat(amountVal);
+  const amountVal = Number.parseFloat(amountEl.value);
 
+  // Card function invoking
   makeCards(descVal, amountVal);
 
   if (amountVal < 0) {
+    // Computing expenses
     totalExpense += Math.abs(amountVal);
   } else {
+    // Computing incomes
     totalIncome += amountVal;
   }
 
+  // Computing main balance
   let mainBalance = totalIncome - totalExpense;
   if (mainBalance < 0) {
     mainBalance = 0;
   }
 
+  // Manipulating DOM
   totalBalanceEl.textContent = mainBalance;
   totalIncomeEl.textContent = totalIncome;
   totalExpenseEl.textContent = totalExpense;
-  // descEl.value = "";
-  // amountEl.value = "";
+
+  // Blanking inputs
+  descEl.value = "";
+  amountEl.value = "";
 }
 
-const button = document.getElementById("transaction-btn");
+// Button listener
 button.addEventListener("click", addTransaction);
 
 // Preventing from reload
-const form = document.querySelector(".transaction-form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
